f6f8f8bc889f77340874030842d7c364e3807453
Plamen Milanov <plamenpk@gmail.com>
2024-06-07 18:07:41 +0200

    refactored style Home page

--- mygames/app/page.tsx
+++ mygames/app/page.tsx
@@ -15,7 +15,7 @@ const Home = () => {
             <p className="py-8">Try & enjoy games with all over the world.</p>
           </div>
           <div className="text-lg flex gap-4">
-            <Link href="/community" className="mt-4 px-4 py-2 rounded-lg bg-gradient-to-r rounded from-[#81d8f3] to-[#2b8af7] text-white font-bold no-underline">Join the Community</Link>
+            <Link href="/community" className="mt-4 px-4 py-2 bg-gradient-to-r rounded from-[#77d5f1] to-[#2b8af7] text-white font-bold no-underline">Join the Community</Link>
             <Link href="/meals" className="mt-4 px-4 py-2 bg-transparent font-normal text-blue-900 border rounded border-blue-600 no-underline">Explore Games</Link>
           </div>
         </div>
