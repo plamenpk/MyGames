bc5374a8cd08ec219108ed7fb7e5726257377a6f
Plamen Milanov <plamenpk@gmail.com>
2024-06-09 18:58:31 +0200

    Add Card Form

--- mygames/app/games/page.tsx
+++ mygames/app/games/page.tsx
@@ -1,7 +1,25 @@
-const Games = () => {
+import CardForm from "@/components/UI/CardForm";
+import sudokuImg1 from "@/assets/tic-tac-toe1.png";
+import sudokuImg2 from "@/assets/tic-tac-toe2.png";
+
+const games = [
+  { name: 'Sudoku', img: sudokuImg1 },
+  { name: 'Tic Toc Toe', img: sudokuImg2 },
+  { name: 'Game', img: sudokuImg1 },
+  { name: 'Sudoku', img: sudokuImg1 },
+  { name: 'Tic Toc Toe', img: sudokuImg2 }
+]
+
+const GamesPage = () => {
+
   return (
-    <div>All games</div>
+    <>
+      <header></header>
+      <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1">
+        {games.map((game, i) => <CardForm key={i}>{game.name}</CardForm>)}
+      </main>
+    </>
   )
 }
 
-export default Games;
+export default GamesPage;
