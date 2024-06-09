55a57d8163b1d2845ca1297b7d76d2baba78f468
Plamen Milanov <plamenpk@gmail.com>
2024-06-09 14:39:02 +0200

    Add custom  nav Link

---
+++ mygames/components/UI/NavLink.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode, FC } from "react";
+
+
+interface NavLinkProps {
+  href: string;
+  children: ReactNode;
+}
+
+const NavLink: FC<NavLinkProps> = ({ href, children }) => {
+  const ifPath = usePathname().startsWith(href);
+
+  return (
+    <Link
+      href={href}
+      className={ifPath
+        ? "text-blue-800 text-xl px-4 py-2 hover:text-blue-800"
+        : "border rounded border-blue-600 text-blue-500 text-xl px-4 py-2 hover:text-blue-800"
+      }
+    >
+      {children}</Link>
+  )
+}
+
+export default NavLink;
