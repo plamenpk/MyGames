00ad0de524674f71a090d184c7340132d0eba109
Plamen Milanov <plamenpk@gmail.com>
2024-06-11 20:14:54 +0200

    Refactored GameBoard

--- mygames/app/games/ticTocToe/page.tsx
+++ mygames/app/games/ticTocToe/page.tsx
@@ -1,13 +1,35 @@
 'use client'
 import PlayerAlias from '@/components/ticTacToe/PlayerAlias';
 import GameBoard from './../../../components/ticTacToe/GameBoard';
+import Log from '@/components/ticTacToe/Log';
 import { useState } from 'react';
+import { Turn } from '@/common/ticTacToe/interfaces';
 
 const TicTacToe = () => {
   const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState<Turn[]>([]);
 
-  const handleSelectSquare = () => {
-    setActivePlayer(currentPlayer => currentPlayer === 'X' ? 'O' : 'X')
+  const handleSelectSquare = (rowIndex: number, colIndex: number) => {
+
+    setActivePlayer(currentPlayer => currentPlayer === 'X' ? 'O' : 'X');
+
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O'
+      }
+
+      const updatedTurns = [
+        {
+          square: { row: rowIndex, col: colIndex },
+          player: currentPlayer
+        },
+        ...prevTurns,
+      ]
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -17,9 +39,9 @@ const TicTacToe = () => {
           <PlayerAlias initialName="player 1" symbol="X" isActive={activePlayer === 'X'}></PlayerAlias>
           <PlayerAlias initialName="player 2" symbol="O" isActive={activePlayer === 'O'}></PlayerAlias>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        <Log />
       </div>
-      log
     </main>
   )
 }
--- mygames/common/ticTacToe/interfaces.ts
+++ mygames/common/ticTacToe/interfaces.ts
@@ -1,7 +1,15 @@
 interface GameBoardRow extends Array<string | null> { };
 
 export interface GameBoardMatrixProps extends Array<GameBoardRow> { };
+interface Square {
+  row: number;
+  col: number
+}
+export interface Turn {
+  square: Square;
+  player: string
+}
 export interface GameBoardProps {
-  onSelectSquare: () => void;
-  activePlayerSymbol: string
+  onSelectSquare: (rowIndex: number, colIndex: number) => void;
+  turns: Turn[]
 }
