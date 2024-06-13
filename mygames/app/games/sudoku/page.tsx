a0b6e32e88ca2851ed5e6a9a47744ab0ca594016
Plamen Milanov <plamenpk@gmail.com>
2024-06-13 18:39:34 +0200

    Add dynamic path

--- mygames/app/games/sudoku/page.tsx
+++ mygames/app/games/sudoku/page.tsx
@@ -1,5 +1,6 @@
 import { GAMES } from "@/common/sudoku/data";
 import CardForm from "@/components/UI/CardForm";
+import Link from "next/link";
 
 const Sudoku = () => {
   return (
@@ -8,7 +9,8 @@ const Sudoku = () => {
         <h1> Sudoku</h1>
       </header>
       <main className="m-6 px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-
+        <p><Link href='/games/sudoku/sudoku1'>Sudoku1</Link></p>
+        <p><Link href='/games/sudoku/sudoku2'>Sudoku2</Link></p>
         {GAMES.map((game, i) => <CardForm key={i} slug="/games/sudoku/singleSudokuGame" id={game.id} img={game.img}>{game.name}</CardForm>)}
       </main>
     </>
