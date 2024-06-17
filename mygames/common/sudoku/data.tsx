008c94436c74df7a2b95dab37746a5888137637c
Plamen Milanov <plamenpk@gmail.com>
2024-06-17 13:15:50 +0200

    Refactored sudokuBoard

--- mygames/common/sudoku/data.tsx
+++ mygames/common/sudoku/data.tsx
@@ -1,9 +1,33 @@
 import sudokuImg1 from "@/assets/tic-tac-toe1.png";
 import sudokuImg2 from "@/assets/tic-tac-toe2.png";
-export const GAMES  = [
+export const GAMES = [
   { id: 'id1', name: 'Sudoku', img: sudokuImg1 },
   { id: 'id2', name: 'Tic Toc Toe', img: sudokuImg2 },
   { id: 'id3', name: 'Game', img: sudokuImg1 },
   { id: 'id4', name: 'Sudoku', img: sudokuImg1 },
   { id: 'id5', name: 'Tic Toc Toe', img: sudokuImg2 }
+]
+export const sudokuTemplates = [
+  [
+  [1, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [1, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, 1, null, null],
+  [null, null, null, null, null, null, null, 6, null],
+  [1, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null]
+],
+[
+  [2, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, 2, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [2, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, 2, null, null],
+  [null, null, null, null, null, null, null, 6, null],
+  [2, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null]
+]
 ]
