cd02cfdec62cdf9dad907d79838f6d46b89e3f43
Plamen Milanov <plamenpk@gmail.com>
2024-06-18 12:15:44 +0200

    Add counter

---
+++ mygames/app/counter/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment, selectValue, incrementByAmount } from '@/slices/counterSlice';
+import { useState } from 'react';
+
+const Counter = () => {
+  const [amount, setAmount] = useState(0);
+  const count = useSelector(selectValue);
+  const dispatch = useDispatch();
+  console.log(amount)
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number(e.target.value));
+    dispatch(incrementByAmount(Number(e.target.value)))
+  };
+  return (
+    <div>
+      <div className='flex justify-center items-center'>
+        <button onClick={() => dispatch(increment())} className='w-full h-10 bg-green-400'>Increment</button>
+        <h1> Counter={count}</h1>
+        <button onClick={() => dispatch(decrement())} className='w-full h-10 bg-red-400'>Decrement</button>
+      </div>
+        <input className='w-full h-10 bg-yellow-400' type='text' onChange={e => setAmount(Number(e.target.value))}></input>
+      <button onClick={() => dispatch(incrementByAmount(amount))} className='w-full h-10 bg-blue-300'>
+        increment by{amount}
+      </button>
+      <input
+          type='number'
+          value={amount}
+          onChange={handleInputChange}
+          className='w-full h-10 bg-green-300'
+        />
+    </div>
+  )
+}
+
+export default Counter;
---
+++ mygames/slices/counterSlice.ts
@@ -0,0 +1,33 @@
+import { RootState } from '@/store/store'
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface CounterState {
+  value: number
+}
+
+const initialState: CounterState = {
+  value: 3,
+}
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+          state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const selectValue = (state: RootState) => state.counter.value;
+
+export default counterSlice.reducer
