f9379df18ea3e2d3e412a12bc8fcbc7cb7df5958
Plamen Milanov <plamenpk@gmail.com>
2024-10-09 18:18:38 +0300

    Refactored style Login Form

--- mygames/components/LoginForm.tsx
+++ mygames/components/LoginForm.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { FormEvent, useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import logoImg from "@/assets/tic-tac-toe2.png";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
@@ -40,24 +42,26 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="grid place-items-center h-screen">
-      <div className="shadow-lg p-5 rounded-lg border-t-8 border-r-8 border-green-400">
-        <h1 className="text-xl font-bold my-4">Login</h1>
-
+    <div className="grid place-items-center pt-6">
+      <div className="shadow-lg px-32 py-20 rounded-sm border border-blue-600 bg-white">
+        <h1 className="text-xl my-3 text-center">Login</h1>
+        <Image src={logoImg} alt="" priority className='mx-auto my-3 w-16 h-16' />
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input
+            className='border rounded-sm border-blue-600'
             onChange={(e) => setEmail(e.target.value)}
             type="text"
             placeholder="Email"
             required
           />
           <input
+            className='border rounded-sm border-blue-600'
             onChange={(e) => setPassword(e.target.value)}
             type="password"
             placeholder="Password"
             required
           />
-          <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-medium px-6 py-2 cursor-pointer">
             Login
           </button>
           {error && (
@@ -67,7 +71,7 @@ const LoginForm = () => {
           )}
 
           <Link className="text-sm mt-3 text-right" href={"/register"}>
-            Don&apos;t have an account? <span className="underline">Register</span>
+            Don&apos;t have an account? <span className="underline text-blue-500">Register</span>
           </Link>
         </form>
       </div>
