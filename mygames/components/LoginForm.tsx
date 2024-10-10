3259f29df6afd9ead7cef71ab96805c5f781b2c4
Plamen Milanov <plamenpk@gmail.com>
2024-10-10 19:30:59 +0300

    Refactored LoginForm

--- mygames/components/LoginForm.tsx
+++ mygames/components/LoginForm.tsx
@@ -2,15 +2,20 @@
 
 import Link from "next/link";
 import { FormEvent, useState } from "react";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import logoImg from "@/assets/tic-tac-toe2.png";
+import { setUserState, resetUserState, selectValue } from "@/slices/userStateSlice";
+import { useDispatch } from "react-redux";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+
+  const { data: session } = useSession(); // useSession hook to get session
+  const dispatch = useDispatch();
 
   const router = useRouter();
 
@@ -34,17 +39,21 @@ const LoginForm = () => {
         return;
       }
 
-      router.replace("dashboard");
+      router.replace('/');
     } catch (error) {
       console.log('signIn', error);
       setError("An error occurred. Please try again.");
     }
   };
 
+  if (session?.user?.name) {
+    dispatch(setUserState(session.user.name));
+  }
+
   return (
     <div className="grid place-items-center pt-6">
       <div className="shadow-lg px-32 py-20 rounded-sm border border-blue-600 bg-white">
-        <h1 className="text-xl my-3 text-center">Login</h1>
+        <h1 className="text-xl my-3 text-center">Log In</h1>
         <Image src={logoImg} alt="" priority className='mx-auto my-3 w-16 h-16' />
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input
@@ -62,7 +71,7 @@ const LoginForm = () => {
             required
           />
           <button className="bg-blue-500 hover:bg-blue-600 text-white font-medium px-6 py-2 cursor-pointer">
-            Login
+            Log In
           </button>
           {error && (
             <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
