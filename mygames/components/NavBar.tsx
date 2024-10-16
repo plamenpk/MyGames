6815ec6cba7d8baffe91fcdb5e4d091236ba5311
Plamen Milanov <plamenpk@gmail.com>
2024-10-16 09:06:22 +0300

    Refactored style

--- mygames/components/NavBar.tsx
+++ mygames/components/NavBar.tsx
@@ -83,7 +83,7 @@ const NavBar = () => {
         <div className="flex lg:flex-1 text-blue-900 text-3xl font-bold font-family: Montserrat, serif uppercase">
           <Link href="/" className="flex items-center justify-center gap-8 -m-1.5 p-1.5">
             {/* <span className="sr-only">Your Company</span> */}
-            <Image className="w-14 h-14" src={logoImg} alt="" priority />
+            <Image className="w-12 h-14" src={logoImg} alt="" priority />
             My Next Level Games
           </Link>
         </div>
