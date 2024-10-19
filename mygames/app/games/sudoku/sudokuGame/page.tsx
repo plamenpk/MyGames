cd9ef360d6a94e66c4fa9862552b37cfbb7de189
Plamen Milanov <plamenpk@gmail.com>
2024-10-19 17:32:39 +0200

    Refactored style

--- mygames/app/games/sudoku/sudokuGame/page.tsx
+++ mygames/app/games/sudoku/sudokuGame/page.tsx
@@ -1,14 +1,10 @@
-import { newSudokuBoard } from "@/common/sudoku/data";
-import { SudokuPageProp } from "@/common/sudoku/interfaces";
 import SudokuBoard from "@/components/sudoku/SudokuBoard";
 
 const SudokuGame: React.FC = () => {
-  // const board = sudokuTemplates[+params.sudokuSlug - 1]
 
-const board = newSudokuBoard
   return (
     <div>
-      <SudokuBoard board={board} />
+      <SudokuBoard />
     </div>
   );
 };
