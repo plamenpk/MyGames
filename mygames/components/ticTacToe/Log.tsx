57c9ec88f20c0f80da4089a22886c5703b88c3a4
Plamen Milanov <plamenpk@gmail.com>
2024-10-22 15:22:45 +0200

    Add player name

--- mygames/components/ticTacToe/Log.tsx
+++ mygames/components/ticTacToe/Log.tsx
@@ -1,12 +1,11 @@
 import { LogProps } from "@/common/ticTacToe/interfaces";
 
-
-const Log: React.FC<LogProps> = ({ turns }) => {
+const Log: React.FC<LogProps> = ({ turns, players }) => {
   return (
     <ol className="max-w-sm text-[#3f3b00] list-none my-8 mx-auto p-0 text-center">
       {turns.map(turn => (
         <li key={`${turn.square.row}${turn.square.col}`}>
-          {turn.player} selected {turn.square.row+1}, {turn.square.col+1}
+          {players[turn.player]} selected {turn.square.row+1}, {turn.square.col+1}
         </li>
       ))}
     </ol>
