ed6f9239673f8b941e56560b9b34d361c7ebee64
Plamen Milanov <plamenpk@gmail.com>
2024-10-22 22:55:32 +0200

    Add input validation

--- mygames/components/sudoku/SudokuBoard.tsx
+++ mygames/components/sudoku/SudokuBoard.tsx
@@ -14,8 +14,12 @@ const SudokuBoard: FC = () => {
   const gameBoard = useSelector(selectBoard);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, row: number, col: number) => {
-    const value = e.target.value === '' ? null : Number(e.target.value);
-    dispatch(updateCell({ row, col, value: value }));
+    const value = e.target.value;
+    
+    if (value === '' || /^[1-9]$/.test(value)) {
+      const numericValue = e.target.value === '' ? null : Number(e.target.value);
+      dispatch(updateCell({ row, col, value: numericValue }));
+    }
   };
 
   const handleOnClick = (rowIndex: number, colIndex: number) => {
@@ -37,10 +41,10 @@ const SudokuBoard: FC = () => {
 
   return (
     <SudokuGrid
-    gameBoard={gameBoard}
-    handleInputChange={handleInputChange}
-    handleOnClick={handleOnClick}
-  />
+      gameBoard={gameBoard}
+      handleInputChange={handleInputChange}
+      handleOnClick={handleOnClick}
+    />
   )
 };
 
