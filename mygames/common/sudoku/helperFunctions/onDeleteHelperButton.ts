9f0850bdfa7b9897f3e662c90ffe1ae34d74073b
Plamen Milanov <plamenpk@gmail.com>
2024-10-23 22:06:34 +0200

    Deleted comments

--- mygames/common/sudoku/helperFunctions/onDeleteHelperButton.ts
+++ mygames/common/sudoku/helperFunctions/onDeleteHelperButton.ts
@@ -1,14 +1,3 @@
-// import { useDispatch } from "react-redux";
-// import { resetSelectedNumber } from "@/slices/selectedNumberSlice";
-
-// export const onDeleteHelperButton = () => {
-//   const dispatch = useDispatch();
-//   dispatch(resetSelectedNumber());
-
-// };
-
-// File: /path/to/your/helperFunctions/onDeleteHelperButton.ts
-
 import { useDispatch } from "react-redux";
 import { resetSelectedNumber } from "@/slices/selectedNumberSlice";
 
