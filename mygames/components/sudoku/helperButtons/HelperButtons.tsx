c3c6503caa106f180bb74101e6703e6c4c70378c
Plamen Milanov <plamenpk@gmail.com>
2024-10-24 21:52:34 +0200

    Add onUndoClick func

--- mygames/components/sudoku/helperButtons/HelperButtons.tsx
+++ mygames/components/sudoku/helperButtons/HelperButtons.tsx
@@ -1,15 +1,17 @@
 'use client'
 import HelperButton from "./HelperButton";
 import { useOnDeleteHelperButton } from "@/common/sudoku/helperFunctions/onDeleteHelperButton";
+import { useOnUndoClick } from "@/common/sudoku/helperFunctions/onUndoClick";
 import { onHintClick } from "@/common/sudoku/helperFunctions/onHintClick";
 
 import { ArrowUturnLeftIcon, ArrowUturnRightIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const HelperButtons = () => {
   const { onDeleteHelperButton} = useOnDeleteHelperButton();
+  const {onUndoClick} = useOnUndoClick();
   return (
     <div className="grid grid-cols-4 gap-0">
-      <HelperButton handleOnClick={onDeleteHelperButton}><ArrowUturnLeftIcon /></HelperButton>
+      <HelperButton handleOnClick={onUndoClick}><ArrowUturnLeftIcon /></HelperButton>
       <HelperButton handleOnClick={onDeleteHelperButton}><ArrowUturnRightIcon /></HelperButton>
       <HelperButton handleOnClick={onDeleteHelperButton}><XMarkIcon /></HelperButton>
       <HelperButton handleOnClick={onHintClick}>Hint...</HelperButton>
