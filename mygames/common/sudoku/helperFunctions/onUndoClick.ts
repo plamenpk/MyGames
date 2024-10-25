3312742ff49c40ca7ed1e2196d65a64875f1a3b5
Plamen Milanov <plamenpk@gmail.com>
2024-10-25 15:27:42 +0200

    Refactored onUndoClick

--- mygames/common/sudoku/helperFunctions/onUndoClick.ts
+++ mygames/common/sudoku/helperFunctions/onUndoClick.ts
@@ -1,11 +1,12 @@
 import { useDispatch } from "react-redux";
 import { setSelectedButton } from "@/slices/selectedButtonSlice";
+import { UNDO } from "../constants";
 
 export const useOnUndoClick = () => {
   const dispatch = useDispatch();
 
   const onUndoClick = () => {
-    dispatch(setSelectedButton('undo'))
+    dispatch(setSelectedButton(UNDO))
   };
   return { onUndoClick };
 }
