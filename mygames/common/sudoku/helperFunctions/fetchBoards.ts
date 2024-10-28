014b739e8e00f5f6de0dca0c236e4e7b04dd0a05
Plamen Milanov <plamenpk@gmail.com>
2024-10-28 10:54:39 +0100

    Refactored return fetchBoards

--- mygames/common/sudoku/helperFunctions/fetchBoards.ts
+++ mygames/common/sudoku/helperFunctions/fetchBoards.ts
@@ -1,12 +1,12 @@
 import { NewSudokuBoard } from "@/models/sudokuBoard";
-import { Dispatch } from "react";
 
-export  const fetchBoards = async (setBoards: Dispatch<any>) => {
+export  const fetchBoards = async () => {
   try {
-    const response = await fetch('/api/newGame', { method: 'POST' });
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
+    const response = await fetch(`${baseUrl}/api/newGame`, { method: 'POST' });
     const data: NewSudokuBoard[] = await response.json();
     const arrOfBoards = data.map((obj: NewSudokuBoard) => obj.board.board);
-    setBoards(arrOfBoards);
+    return arrOfBoards;
   } catch (error) {
     console.error('Failed to fetch boards', error);
     console.log(error)
